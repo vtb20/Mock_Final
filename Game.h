4a30a080df4d303bb7094a9bd3462b604047c7f3
vtb20 <130160027+vtb20@users.noreply.github.com>
2024-07-08 20:27:27 +0700

    Update Game.h

--- Game.h
+++ Game.h
@@ -1,10 +1,11 @@
-﻿// Game.h
 #ifndef GAME_H
 #define GAME_H
 
 #include <iostream>
 #include "Board.h"
 #include "Player.h"
+#include "Bot.h"
+#include "Replay.h"
 using namespace std;
 
 class Game {
@@ -13,18 +14,20 @@ private:
     Player* p1;
     Player* p2;
     int turn;
+    Replay replay;
 
 public:
-    Game();
     Game(const Player& player1, const Player& player2);
+    Game(const Player& player1, const Bot& bot);
     void playWithOtherPlayer();
-    void showGuide() const;
-    void showInfoPlayer(const string& name) const;
-    //void processMove(int x, int y);
-    //bool checkWin();
-    //void showBoard();
-    //void loadReplay(const std::string& filename);
-    //void saveReplay(const std::string& filename);
+    void playVsBot();
+    static void playReplay();
+    static void viewReplay();
+    void saveReplay() ;
+    void loadReplay(const string& filename);
+    static vector<string> getReplayList();
+    static pair<string, string> getPlayerNamesFromReplayFile(const string& filename);
+
 };
 
 #endif // GAME_H
